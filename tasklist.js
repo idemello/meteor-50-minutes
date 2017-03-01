8bce7c72267074f89ecb06e99e3dc7bbf8af324d
idemello <idemello@hawaii.edu>
2017-03-01 13:44:24 -1000

    Task 4: Tasklist authentication

---
+++ tasklist.js
@@ -0,0 +1,44 @@
+Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isClient) {
+  Template.tasks.helpers({
+    tasks: function() {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.tasks.events({
+    "submit .add-task": function(event) {
+      let name = event.target.name.value;
+      Meteor.call('addTask', name);
+      event.target.name.value = '';
+      return false;
+    },
+
+    "click .delete-task": function(event) {
+      if (confirm('Delete Task?')) {
+        Meteor.call('deleteTask', this._id);
+      }
+      return false;
+    }
+  })
+}
+
+if (Meteor.isServer) {
+
+}
+
+Meteor.methods({
+  addTask: function(name){
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('No Access');
+    }
+    Tasks.insert({
+      name: name, createdAt: new Date(), userId: Meteor.userId()
+    });
+  },
+
+  deleteTask: function(taskId) {
+    Tasks.remove(taskId);
+  }
+})
